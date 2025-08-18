cab1ce8138bb303d9f1163627048de6ac6ddb03c
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-08-18 10:30:10 -0300

    feat: add scripts and others configs

---
+++ scripts/i3_detect_displays.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Only start this up when this isn't already going.
+if xrandr | grep '^HDMI-1.* connected'; then
+    MAIN_DISPLAY=$(xrandr | grep '^HDMI-1.* connected' | awk '{print $1;}')
+    LAPTOP_DISPLAY=$(xrandr | grep '^eDP-1.* connected' | awk '{print $1;}')
+    xrandr --newmode "2560x1080x49.94"  150.25  2560 2608 2640 2720  1080 1083 1087 1106  +HSync -VSync
+    xrandr --addmode $MAIN_DISPLAY 2560x1080x49.94
+    xrandr --output $LAPTOP_DISPLAY --mode 1920x1080 --pos 2561x0 --rotate normal --output $MAIN_DISPLAY --primary --mode 2560x1080x49.94 --pos 0x0 --rotate normal
+else
+
+  xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output VIRTUAL-1 --off
+fi
+
