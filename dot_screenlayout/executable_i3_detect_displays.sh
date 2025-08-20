48de070532d0e6ebf58b7582fa17d8420120b37c
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-08-20 02:08:19 -0300

    fix: fix displays and keyboard

--- dot_screenlayout/executable_i3_detect_displays.sh
+++ dot_screenlayout/executable_i3_detect_displays.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+sleep 3
 # Only start this up when this isn't already going.
 if xrandr | grep '^HDMI-1.* connected'; then
     MAIN_DISPLAY=$(xrandr | grep '^HDMI-1.* connected' | awk '{print $1;}')
     LAPTOP_DISPLAY=$(xrandr | grep '^eDP-1.* connected' | awk '{print $1;}')
+
+    # Garante que o mode existe (remove se já existir para evitar erro)
+    xrandr | grep -q '2560x1080x49.94' && xrandr --rmmode 2560x1080x49.94
     xrandr --rmmode "2560x1080x49.94"
     xrandr --newmode "2560x1080x49.94"  150.25  2560 2608 2640 2720  1080 1083 1087 1106  +HSync -VSync
     xrandr --addmode $MAIN_DISPLAY 2560x1080x49.94
